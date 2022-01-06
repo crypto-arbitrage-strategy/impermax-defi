8a5eeb25483c834f16cdd7db505365068328da52
周寅吉 <tiresias@zhouyinjis-MacBook-Pro.local>
2022-01-06 13:47:47 +0000

    swap

--- modules/swap.py
+++ modules/swap.py
@@ -6,7 +6,7 @@
 import time
 from utils import web3obj, to_checksum_address, read_json
 from abc import ABC
-import web3
+from web3 import Web3
 from settings import PRIVATE_KEY, ADDRESS
 
 
@@ -32,8 +32,8 @@ class Solarbean(Swap):
 
     def swap_solar_for_eth(self, amount_in: int,
                            sender_address: str, amount_out_min: int = 0,
-                           path: list = ["6bd193ee6d2104f14f94e2ca6efefae561a4334b",
-                                         "98878b06940ae243284ca214f92bb71a2b032b8a"],
+                           path: list = [Web3.toChecksumAddress("6bd193ee6d2104f14f94e2ca6efefae561a4334b"),
+                                         Web3.toChecksumAddress("98878b06940ae243284ca214f92bb71a2b032b8a")],
 
                            ) -> dict:
 
@@ -52,19 +52,21 @@ class Solarbean(Swap):
             amount_in = int(amount_in * 1e18)
             amount_out_min = int(amount_out_min * 1e18)
             nonce = self.web3.eth.get_transaction_count(sender_address)
-            txn = self.router_contract.functions.SwapExactTokensForEth(amount_in, amount_out_min, path,
-                                                                       deadline=deadline,
-                                                                       to=sender_address, ).buildTransaction({
+            txn = self.router_contract.functions.swapExactTokensForETH(amount_in, amount_out_min, path,
+                                                                       to=sender_address,
+                                                                       deadline=int(deadline),
+                                                                       ).buildTransaction({
                 'from': sender_address,
-                'value': self.web3.toWei(amount_in, 'ether'),
+                'value': amount_in,
                 'gas': 50000,
                 'gasPrice': self.web3.eth.gasPrice,
                 'nonce': nonce,
             })
-            signed_txn = self.web3.eth.account.sign_transaction(txn, self.private_key)
-            tx_token = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-            transaction = (self.web3.toHex(tx_token))
-            return {200: f'success: {transaction}'}
+            signed_tx = self.web3.eth.account.sign_transaction(txn, self.private_key)
+            txn_hash = self.web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+            print("Waiting for confirmation")
+            txn_receipt = self.web3.eth.wait_for_transaction_receipt(txn_hash)
+            return {200: f'success: {txn_receipt}'}
         except Exception as e:
             return {400: e}
 
@@ -79,7 +81,8 @@ class Solarbean(Swap):
     #                                                          to=sender_address, ).call()
     #
 
+
 if __name__ == '__main__':
     solarbean = Solarbean(PRIVATE_KEY, ADDRESS)
     print(solarbean.get_all_functions())
-    print(solarbean.swap_solar_for_eth(amount_in=0.001, sender_address=ADDRESS))
+    print(solarbean.swap_solar_for_eth(amount_in=0.0001, sender_address=ADDRESS))
