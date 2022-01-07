d4b479c95d0db2c95fcf9ee2ed35a218abb5d644
mattlim96-Desktop <mattlim96@gmail.com>
2022-01-07 19:59:06 +0000

    Minor update

--- modules/query/bounty_query.py
+++ modules/query/bounty_query.py
@@ -6,23 +6,25 @@
 # @Desc     : This module is used to query current bounty of impermax
 #%%
 from web3 import Web3
-# from utils import web3obj, read_json
+from utils import web3obj, read_json, to_checksum_address
 import os
 import logging
 
 class BountyQuery:
-    def __init__(self, provider_url):
-        self.web3 = self.connect_web3(provider_url)
-        self.abi = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../abi/', 'impermax.json'))
+    def __init__(self):
+    # def __init__(self, provider_url):
+        # self.web3 = self.connect_web3(provider_url)
+        # self.abi = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../abi/', 'impermax.json'))
         
-        # self.web3 = web3obj
-        # self.abi = read_json('abi/impermax.json')
+        self.web3 = web3obj
+        self.abi = read_json('abi/impermax.json')
 
         self.contract_dict = {'0x899caad2d29eff3b628503a85dae736b5c4d1671':'ETH/MOVR',
                               '0x35e8f2d87821a5149d92833390880f65911ad4d0':'FRAX/MOVR',
                               '0x2748d9d4e7379d5d29ca8887aeff929912ff06d8':'MIM/MOVR',
                               '0x24e19bd8477077b80de25ee7fc9f7a67d89d8009':'WBTC/MOVR'}
-        self.test_contract = self.web3.eth.contract(address=Web3.toChecksumAddress([*self.contract_dict.keys()][0]), abi=open(self.abi).read())
+        # self.test_contract = self.web3.eth.contract(address=Web3.toChecksumAddress([*self.contract_dict.keys()][0]), abi=open(self.abi).read())
+        self.test_contract = self.web3.eth.contract(address=to_checksum_address('0x899caad2d29eff3b628503a85dae736b5c4d1671'), abi=open(self.abi).read())
 
     def connect_web3(self, provider_url):
         web3 = Web3(Web3.HTTPProvider(provider_url))
@@ -47,8 +49,8 @@ class BountyQuery:
 
 #%%
 if __name__ == "__main__":
-    query = BountyQuery('https://moonriver.api.onfinality.io/public')
-    # query = BountyQuery()
+    # query = BountyQuery('https://moonriver.api.onfinality.io/public')
+    query = BountyQuery()
 
     query.get_all_bounties()
 
