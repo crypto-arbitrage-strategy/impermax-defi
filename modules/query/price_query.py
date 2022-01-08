5e4777c8bad409d61469cc87e9f3fb147513b234
Tim Liu <tim.kuantingliu@gmail.com>
2022-01-08 16:07:59 +0000

    Finished price query

--- modules/query/price_query.py
+++ modules/query/price_query.py
@@ -6,40 +6,47 @@
 # @Desc     : This module is used to query the price of a crypto
 
 #%%
-import ccxt
-#%% Base class
-class PriceQueryBase:
-    def __init__(self, exchange_id: str, timeout = 30000, enableRateLimit = True):  #, currency
-#         self.currency = currency
-        self.exchange_id = exchange_id
-        self.timeout = timeout
-        self.enableRateLimit = enableRateLimit
-#         self.adapt() 
-        
+from utils import web3obj, to_checksum_address, read_json
+from abc import ABC
+from web3 import Web3
+from settings import PRIVATE_KEY, ADDRESS
 
-#     def adapt(self):
-#         self.currency = self.currency.replace("_", "")
+#%%
+class Query():
+    def __init__(self, private_key, address):
+        self.web3 = web3obj 
+        self.router_contract = None 
+        self.private_key = private_key
+        self.address = address
+        self.abi = read_json('solarbeam.json')
+        contract = '0xaa30ef758139ae4a7f798112902bf6d65612045f'
+        self.contract = self.web3.eth.contract(address=to_checksum_address(contract),abi=self.abi)
+        
+        basetoken = '0x98878B06940aE243284CA214f92Bb71a2b032B8A' #WETH
+        selltoken = '0x6bD193Ee6D2104F14F94E2cA6efefae561A4334B' #SOLAR
+        self.basetoken = basetoken
+        self.selltoken = selltoken
+
+
     
-    def query(self,currency) -> float:
+    def price_query(self,amount_in) -> float:
         """
         Query the price of a crypto currency
         :param currency:
         :return:
-            the price of the crypto currency in USD/USDC/USDT
+            the price of the crypto currency in movr
         """
-        
-        exchange_class =getattr(ccxt, self.exchange_id)
-        exchange = exchange_class({
-        'timeout':self.timeout,
-        'enableRateLimit':self.enableRateLimit,
-        })
-        
-        ticker_name = exchange.fetch_ticker(currency)
-        name_price = float(ticker_name['last'])
-        print(name_price)
-#%% choose exchange id
-lbank = PriceQueryBase('lbank')
-#%% query price output
-print(lbank.query("SOLAR/USDT"))
-print(lbank.query("SUSHI/USDT"))
-# %%
+        baseCurrency = self.web3.toChecksumAddress(self.basetoken)    #MOVR or WETH
+        tokenToSell = self.web3.toChecksumAddress(self.selltoken)     #SOLAR
+
+        #Calculate minimum amount of tokens to receive
+        amountOut = self.contract.functions.getAmountsOut(10**18, [tokenToSell, baseCurrency],fee=30).call()
+        amountOutMin = amount_in * float((self.web3.fromWei(int(amountOut[1]),'ether')))
+        return amountOutMin #Minimum tokens to recieve
+
+
+#%%
+if __name__ == '__main__':
+    Query = Query(PRIVATE_KEY, ADDRESS)
+    print(Query.price_query(amount_in=1.3))
+
